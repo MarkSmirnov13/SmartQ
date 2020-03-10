0f3238729cffecfce512827f887356fbc5e92509
Maksim <Lukianchukma@gmail.com>
2020-03-10 15:01:07 +0300

    бяка

--- client/src/pages/QueuePage.js
+++ client/src/pages/QueuePage.js
@@ -1,23 +1,32 @@
-import React from "react"
-import { useDispatch } from "react-redux"
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import io from "socket.io-client"
 
-import { enterQueue } from "../store/actions/queue"
-import { useForm } from "../hooks"
+import { addMember } from "../store/actions/queue"
+import { useForm, useLocalStorage } from "../hooks"
 
 export const QueuePage = () => {
   const dispatch = useDispatch()
   const socket = io.connect("http://localhost:5000")
   const { id } = useParams()
   const [values, handleChange] = useForm({ name: "" })
-  // const [uuid, setUuid] = useLocalStorage("smartQ_uuid", null)
+  const [uuid, setUuid] = useLocalStorage("smartQ_uuid", null)
+  const { members } = useSelector(state => state.queue.queue)
+  console.log(members)
+
+  useEffect(() => {
+    socket.on("memberAdded", member => {
+      console.log(member)
+      dispatch(addMember(member))
+      setUuid(member.uuid)
+    })
+  })
 
   const enterQueueHandler = async event => {
     event.preventDefault()
     try {
-      socket.emit("enterQueue", { name: values.name, uuid: "123456" })
-      dispatch(enterQueue(socket))
+      socket.emit("addMember", { queueId: id, name: values.name, uuid })
     } catch (err) {
       console.error(err)
     }
@@ -29,6 +38,9 @@ export const QueuePage = () => {
       <form onSubmit={enterQueueHandler}>
         <input name="name" value={values.name} placeholder="Ваше имя:" onChange={handleChange} />
         <button type="submit">Войти в очередь!</button>
+        {members.map(el => (
+          <div key={el.uuid}>{el.name}</div>
+        ))}
       </form>
     </div>
   )
--- server/src/onConnectionHandler.js
+++ server/src/onConnectionHandler.js
@@ -1,4 +1,5 @@
 import queueModel from "./database/models/Queue"
+import { generateUuid } from "./database/queries/utils"
 
 export const onConnectionHandler = socket => {
   console.log(`Connected to Socket!! ${socket.id}`)
@@ -15,5 +16,25 @@ export const onConnectionHandler = socket => {
         socket.emit("queueLoaded", result)
       }
     })
+  })
+
+  socket.on("addMember", ({ queueId, name, uuid }) => {
+    const memberUuid = uuid || generateUuid()
+    const newMember = {
+      uuid: memberUuid,
+      name
+    }
+    queueModel.findOneAndUpdate(
+      { uuid: queueId },
+      { $push: { members: newMember } },
+      { new: true, upsert: true },
+      error => {
+        if (error) {
+          console.log(error)
+        } else {
+          socket.emit("memberAdded", newMember)
+        }
+      }
+    )
   })
 }
